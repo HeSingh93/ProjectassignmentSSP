4fe194e8ef3ab56c029faaaa38b8e6e799c43b97
niCognition <Niclas.p1990@gmail.com>
2020-03-26 11:14:17 +0100

    Versus incoming

---
+++ src/main/java/com/example/ssp/controllers/GameRockPaperScissorVersus.java
@@ -0,0 +1,14 @@
+package com.example.ssp.controllers;
+
+import javafx.scene.input.MouseEvent;
+
+public class GameRockPaperScissorVersus {
+    public void rockClicked(MouseEvent mouseEvent) {
+    }
+
+    public void paperClicked(MouseEvent mouseEvent) {
+    }
+
+    public void scissorsClicked(MouseEvent mouseEvent) {
+    }
+}
